32414070c2937e4536f5dea7a176e00b5d9680e2
luis-hernandez01 <58452805+luis-hernandez01@users.noreply.github.com>
2025-07-24 17:29:44 -0500

    Comit update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [ ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+     TypeOrmModule.forRoot({
     type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Cholitotres01',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [
         __dirname + '/**/*.entity{.ts,.js}'
       ],
